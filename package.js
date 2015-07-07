03fbff27eaf6bfa19f3c1f2ed3b154426ea2067b
Mark Goldberg <mgoldberg524@gmail.com>
2015-07-06 20:08:58 -0400

    Initial commit

---
+++ package.js
@@ -0,0 +1,19 @@
+Package.describe({
+  name: 'mgoldberg524:meteor-simple-statistics',
+  version: '0.9.2',
+  summary: 'simple-statistics repackaged for Meteor',
+  git: 'https://github.com/mgoldberg524/meteor-simple-statistics',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+  api.addFiles('lib/simple-statistics/src/simple_statistics.js');
+});
+
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('mgoldberg524:meteor-simple-statistics');
+  api.addFiles('meteor-simple-statistics-tests.js');
+});
