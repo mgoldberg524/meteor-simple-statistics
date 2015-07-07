8b955cdf28bb3d006d1af66c46af44ee3db916b3
Mark Goldberg <mgoldberg524@gmail.com>
2015-07-06 21:25:00 -0400

    Remove meteor default tests

--- package.js
+++ package.js
@@ -10,10 +10,3 @@ Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
   api.addFiles('lib/simple-statistics/src/simple_statistics.js');
 });
-
-
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('mgoldberg524:meteor-simple-statistics');
-  api.addFiles('meteor-simple-statistics-tests.js');
-});
